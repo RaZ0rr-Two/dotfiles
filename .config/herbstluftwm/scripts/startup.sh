2bd116c34cd6ec4b2223f3d9ffa76f71de642f3e
RaZ0rr <rayz0rr@protonmail.com>
2021-12-30 01:56:02 +0530

    nvim repo moved to seperate

--- .config/herbstluftwm/scripts/startup.sh
+++ .config/herbstluftwm/scripts/startup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # Auto screen-locker
-xset s 600
+# xset s 600
+xset s 300 600
 betterlockscreen -u "$HOME/.local/share/lock.png" &
-xss-lock -- betterlockscreen -l --blur 0.0 &
+xss-lock -n dim-screen.sh -- betterlockscreen -l --blur 0.0 &
 # xss-lock -n xscreensaver-command -activate -- betterlockscreen -l --blur 0.0 &
 
 # feh --bg-fill $HOME/Pictures/Walls/Minimal/521028.png
@@ -11,7 +12,7 @@ feh --bg-fill $HOME/.local/share/wall.png
 
 ~/.conky/Mine/branch/ConkybooterBranch &
 
-picom &
+picom --experimental-backends &
 
 # For getting root password dialog prompts when running application that require root priveleges
 # /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
--- .config/nvim/ftplugin/rasi.vim
+++
@@ -1,6 +0,0 @@
-" set syntax=css
-" so $VIMRUNTIME/syntax/css.vim
-" au BufRead,BufNewFile *.rasi		setfiletype css
-" au FileType rasi		setfiletype css
-set commentstring=//\ %s
-" command echom "rasi ftplugin read"
